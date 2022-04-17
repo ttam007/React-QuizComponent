43465b87ba8f31086dffe0784b741b8dd6534551
ttam007 <37131580+ttam007@users.noreply.github.com>
2022-04-16 20:52:10 -0400

    Module 2

---
+++ src/Quiz.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component{
+    render() {
+        return (
+            <div>
+                <div className='QuizQuestion'>
+                    {quizData.quiz_questions[0].instruction_text}                   
+                </div>
+            </div>
+        )
+      };
+
+    constructor (props){
+        super(props);
+        this.state = {quiz_position: 1};
+    }
+
+}
+
+export default Quiz
